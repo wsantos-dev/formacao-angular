58962e81c641cc9b0d11b873458ab8890bf2de14
Wellington Santos <dev.wsantos@outlook.com>
2025-12-22 11:49:13 -0300

    feat: iniciando com o Data Binding

--- meu-primeiro-app/src/app/app.component.ts
+++ meu-primeiro-app/src/app/app.component.ts
@@ -11,65 +11,12 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-    <app-title *ngIf="destruir"></app-title>
-    <br>
-
-    <button (click)="destruirComponent()">Destruir Componente</button>
-    <router-outlet></router-outlet>
+   <app-data-biding></app-data-biding>
+   <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements
-  OnInit,
-  DoCheck,
-  AfterContentInit,
-  AfterContentChecked,
-  AfterViewInit,
-  AfterViewChecked {
-  
+export class AppComponent implements OnInit {
 
-  public valor:number = 1;
-  public destruir: boolean = true;
-
-  constructor(){
-
-  }
-
-  public adicionar(): void{
-    this.valor += 1;
-  }
-
-  public destruirComponent(): void {
-    debugger
-    this.destruir = false;
-  }
-
-  ngOnInit(): void {
-    setTimeout(
-      () => {
-        console.log('Testando o ngOnInit()')
-      }
-      , 5000);
-  }
-  
-  ngDoCheck(): void {
-    debugger
-    console.log('ngDoCheck')
-  }
-  ngAfterContentInit(): void {
-    debugger
-    console.log('ngAfterContentInit')
-  }
-  ngAfterContentChecked(): void {
-    debugger
-    console.log('AfterContentChecked')
-  }
-  ngAfterViewInit(): void {
-    console.log('AfterViewInit')
-  }
-  ngAfterViewChecked(): void {
-    debugger
-    console.log('AfterViewChecked')
-  }
-
+  ngOnInit(): void {}
   
 }
---
+++ meu-primeiro-app/src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.scss']
+})
+export class DataBidingComponent implements OnInit {
+
+  public nome: string = 'Wellington'
+  public idade:number = 40
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
