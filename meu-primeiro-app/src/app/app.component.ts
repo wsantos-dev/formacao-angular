434f4561b7d68fd539264ca630a07f7b30edd7d8
Wellington Santos <dev.wsantos@outlook.com>
2025-12-22 14:53:43 -0300

    feat: Diretivas Estruturais: *ngIf

--- meu-primeiro-app/src/app/app.component.ts
+++ meu-primeiro-app/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-   <app-data-biding></app-data-biding>
+   <app-diretivas-estruturais></app-diretivas-estruturais>
    <router-outlet></router-outlet>
   `
 })
---
+++ meu-primeiro-app/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretivas-estruturais',
+  templateUrl: './diretivas-estruturais.component.html',
+  styleUrls: ['./diretivas-estruturais.component.scss']
+})
+export class DiretivasEstruturaisComponent implements OnInit {
+
+  public condition: boolean = true;
+  public conditionClick: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+     
+     setInterval(() => {
+        if (this.condition){
+          this.condition = false
+        }
+        else {
+          this.condition = true
+        }
+
+     }, 2000)
+  }
+
+  public exibir(){
+    setInterval(() => {
+        if (this.condition){
+          this.conditionClick = false
+        }
+        else {
+          this.conditionClick = true
+        }
+
+     }, 2000)
+  }
+
+}
